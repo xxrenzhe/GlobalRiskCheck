078409a309a1a5f70ab782345a023a2cdcad5ae4
Jason Yu <yj2008ay611@gmail.com>
2026-02-21 01:33:33 +0800

    fix: correct use client directives

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -1,4 +1,4 @@
-\"use client\";
+"use client";
 
 import { getRuntimeConfig } from "@/lib/runtimeConfig";
 import { ScoreResult } from "@/lib/types";
--- components/ScorePanel.tsx
+++ components/ScorePanel.tsx
@@ -1,4 +1,4 @@
-\"use client\";
+"use client";
 
 import { ScoreResult } from "@/lib/types";
 
